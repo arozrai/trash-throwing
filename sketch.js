f97e55c7a9abfae1210e61cfba30d601f2783201
arozrai <72703446+arozrai@users.noreply.github.com>
2020-11-09 19:13:01 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -47,7 +47,7 @@ function draw() {
 
 function keyPressed(){
 	if (keyCode === UP_ARROW){
-		Matter.Body.applyForce(ball1.body,ball1.body.position,{x:10,y:-2})
+		Matter.Body.applyForce(ball1.body,ball1.body.position,{x:1,y:-7})
 	}
 }
 
